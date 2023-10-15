7b3b66009e9790292a989fb03890fc37b1660f9c
Cengizhan <cengizhancivelek@gmail.com>
2023-10-16 01:46:04 +0300

    initial commit

---
+++ Productify/Core/Productify.Domain/Entities/Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Productify.Domain.Entities
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+        public int QuantityInStock { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+     
+        public Product( string name, decimal price, int quantityInStock)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            Price = price;
+            QuantityInStock = quantityInStock;
+            CreatedOn = DateTime.Now;
+        }
+
+    }
+}
---
+++ Productify/Presentation/Productify.MVC/Controllers/ProductController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Productify.Domain.Entities;
+using Productify.Persistance;
+
+namespace Productify.MVC.Controllers
+{
+    public class ProductController : Controller
+    {
+       
+        ProductifyDbContext _context;
+
+        public ProductController()
+        {
+            _context = new();
+        }
+        public IActionResult GetAll()
+        {
+            return View(_context.Products.ToList());
+        }
+
+        [HttpGet]
+        public IActionResult AddProduct()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddProduct(string productName,decimal price, int quantityInStock)
+        {
+            _context.Products.Add(new(productName, price, quantityInStock));
+
+            _context.SaveChanges();
+
+            return View();
+        }
+    }
+}
